33a24347ec3f26f1c905739d6bf160ff73aeae6f
ABYADE-Khadija <khadija.abyade20@gmail.com>
2023-02-13 16:43:47 +0100

    simple cnn & enLearning

--- api/app.py
+++ api/app.py
@@ -14,14 +14,21 @@ api = Api(app)
 
 #http://127.0.0.1:5000/test/data1
 
-loaded_model = pickle.load(open("model.pkl","rb"))
+tree_model = pickle.load(open("models/D_Tree.pkl","rb"))
+rf_model = pickle.load(open("models/R_forest.pkl","rb"))
 
-def pred(data):
+
+def pred(data,algo):
     test=[139,265803.35,0.00,0.00,751669.39,1017472.74, 0 , 1,0,0,0 ,0 ]
     test1=[319,1033878.67,1033878.67,0.0,0.0,0.0,0,0,0,0,1,0]
-    to_predict = np.array(test).reshape(1,12)
+    to_predict = np.array(test1).reshape(1,12)
 
-    result = loaded_model.predict(to_predict)
+    result=[]
+    if(algo=="DT"):
+        result = tree_model.predict(to_predict)
+    elif(algo=="RF"):
+        result = rf_model.predict(to_predict)
+    
     print(" TEST ",result[0])
 
     return result[0]
@@ -29,8 +36,9 @@ def pred(data):
   
 class Search(Resource):
 
-    def get(self, data):
-        res=pred(data)
+    def get(self, data,algo):
+        print(algo)
+        res=pred(data,algo)
 
         if res==1:
             return jsonify(
@@ -49,7 +57,7 @@ class Search(Resource):
 
 
 
-api.add_resource(Search, '/test/<string:data>')
+api.add_resource(Search, '/test/<string:data>/<string:algo>')
 
 
 if __name__ == '__main__':
