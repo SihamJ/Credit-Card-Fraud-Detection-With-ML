442b7af6959e490ae3dc6ea1e5e2967752126e6a
Khadija Abyade <kabyade@hps.int>
2023-03-23 10:14:57 +0100

    update implementation

--- api/app.py
+++ api/app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import pickle
 import numpy as np # working with arrays
 import pandas as pd
-
+from flask import request
 
 
 app = Flask(__name__)
@@ -12,31 +12,31 @@ api = Api(app)
 
 
 #http://127.0.0.1:5000/test/data1
+#test=[139,265803.35,0.00,0.00,751669.39,1017472.74, 0 , 1,0,0,0 ,0 ]
+#test1=[319,1033878.67,1033878.67,0.0,0.0,0.0,0,0,0,0,1,0]
+
 
 tree_model = pickle.load(open("models/D_Tree.pkl","rb"))
 rf_model = pickle.load(open("models/R_forest.pkl","rb"))
 
 
 def pred(data,algo):
-    test=[139,265803.35,0.00,0.00,751669.39,1017472.74, 0 , 1,0,0,0 ,0 ]
-    test1=[319,1033878.67,1033878.67,0.0,0.0,0.0,0,0,0,0,1,0]
-    to_predict = np.array(test1).reshape(1,12)
+    test_data=[  4,  2,  23,  1,  410878,  715848.0,  19,  2976,  44,  20339,  3,  1,  621524,  1,  
+                384,  4,  0.654861,  0.654861, -1.0, -0.5,  0.959493, -0.978148,  2009,  2011,  2009,  2009 
+           ]
+    to_predict = np.array(test_data).reshape(1,26)
 
-    result=[]
-    if(algo=="DT"):
-        result = tree_model.predict(to_predict)
-    elif(algo=="RF"):
-        result = rf_model.predict(to_predict)
-    
+    result = rf_model.predict(to_predict)
     print(" TEST ",result[0])
-
+    
     return result[0]
     
-  
+
 class Search(Resource):
 
     def get(self, data,algo):
-        print(algo)
+        data = request.json 
+        print("hi",data)
         res=pred(data,algo)
 
         if res==1:
