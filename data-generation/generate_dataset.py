c457f549abce1af007568ed543d75416a60aba7b
Khadija Abyade <kabyade@hps.int>
2023-05-17 09:01:08 +0100

    new updates

---
+++ data-generation/generate_dataset.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import numpy as np
+from utils import *
+from rules import make_fraud
+import sys, os
+from termcolor import colored as cl # text customization
+
+"""
+How to run this program:
+
+python generate_dataset.py "root_directory" "original_dataset_name"
+
+"""
+
+print(cl('\nReading file...'))
+
+root = sys.argv[1]
+df = pd.read_excel(root + sys.argv[2], dtype = str)
+
+distinct_names = df['CLASS'].unique()
+df.loc[df['CLASS'].isin(distinct_names[1:]) , 'CLASS'] = 1
+df['CLASS']=df['CLASS'].astype(int)
+
+cases = len(df)
+nonfraud_count = len(df[df.CLASS == 0])
+fraud_count = len(df[df.CLASS == 1])
+fraud_percentage = round(fraud_count/nonfraud_count*100, 2)
+
+print(cl('CASE COUNT', attrs = ['bold']))
+print(cl('--------------------------------------------', attrs = ['bold']))
+print(cl('Total number of cases are {}'.format(cases), attrs = ['bold']))
+print(cl('Number of Non-fraud cases are {}'.format(nonfraud_count), attrs = ['bold']))
+print(cl('Number of fraud cases are {}'.format(fraud_count), attrs = ['bold']))
+print(cl('Percentage of fraud cases is {}%'.format(fraud_percentage), attrs = ['bold']))
+print(cl('--------------------------------------------', attrs = ['bold']))
+
+print('\n')
+
+print(cl('\nSplitting Dataset...'))
+split_count = nonfraud_count-fraud_count
+df_fraud = df[df.CLASS == 1]
+df_non_fraud = df[df.CLASS == 0]
+#df_to_update = pd.DataFrame.copy(df_non_fraud[:(split_count)])
+#convert all normal to fraud cases
+df_to_update = pd.DataFrame.copy(df_non_fraud) 
+
+df_fraud = pd.concat([df_fraud, df_to_update])
+
+
+print(cl('\nSaving non fraud file separately...'))
+df_non_fraud.to_excel(root + 'non_fraud_data.xlsx', index=False)
+
+## temporary
+print(cl('\nTreating fraud dataset...'))
+df_fraud.to_excel(root + 'temp.xlsx', index=False)
+
+## Re-reading the file is necessary to avoid some bugs
+df_fraud = pd.read_excel(root + 'temp.xlsx', dtype=str)
+
+print(cl('\nGenerating fraud instances...'))
+df['V10'] = df['V10'].astype(float)
+max_V10 = get_max_values(df)['V10']
+final_fraud = make_fraud(df_fraud, max_V10)
+final_df = pd.concat([df_non_fraud, final_fraud])
+
+
+print(cl('\nSaving results...'))
+final_fraud.to_excel(root + "final_fraud.xlsx", index=False)
+final_df.to_excel(root + "final.xlsx", index=False)
+os.remove(root + 'temp.xlsx')
---
+++ data-generation/utils.py
@@ -0,0 +1,40 @@
+import numpy as np
+import string
+import random 
+
+"""
+get max values of each column in the dataset
+"""
+def get_max_values(df):
+    columns_num = df.select_dtypes(include=np.number).columns.tolist()
+    max_values = {}
+
+    for v in columns_num:
+        max_values[v] = df[v].max()
+
+    return max_values
+
+"""
+Return a random number not in the list
+"""
+def random_not_in(l, max):
+    res = l[0]
+    while(res in l):
+        res = np.random.randint(0, max)
+    return res
+
+"""
+Generate a random string not in the list
+"""
+def random_string_not_in(l):
+    res = l[0]
+    S = len(l[0])
+    while(res in l):
+        res = ''.join(random.choices(string.digits, k = S))    
+    return res
+
+def random_letter_not_in(l):
+    res = l[0]
+    while(res in l):
+        res = random.choices(string.ascii_uppercase, k = 1)   
+    return res[0]
